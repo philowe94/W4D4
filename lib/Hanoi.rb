92a83fe1374fe499c213e9f44054fb0bfa0ef5c4
Philip Lowe <philip.lowe94@gmail.com>
2021-03-18 15:27:13 -0400

    finished spec

---
+++ lib/Hanoi.rb
@@ -0,0 +1,47 @@
+class Hanoi
+
+    attr_accessor :towers
+
+    def initialize        
+        @towers = [[8, 7, 6, 5, 4, 3, 2, 1] , [] ,[]]
+    end
+
+    def move(from, to)
+        if @towers[from].empty?
+            raise "You cannot take something from an empty array"
+        elsif @towers[to].empty?
+            @towers[to].push(@towers[from].pop)
+        elsif  @towers[from][-1] < @towers[to][-1]
+            @towers[to].push(@towers[from].pop)
+        else
+            raise "Cannot move something bigger on top of something smaller"
+        end
+    end
+
+    def won?
+        return @towers[2] == [8,7,6,5,4,3,2,1]
+    end
+
+    def turn
+        render
+        print " Please enter the index of the tower you want to take from: "
+        input = gets.chomp
+        print " Please enter the index of the tower you want to push to: "
+        input2 = gets.chomp
+        move(input.to_i, input2.to_i)
+
+    end
+
+    def render
+        print @towers
+    end
+
+    def play
+        until won?
+            turn
+        end
+    end
+end
+
+# hanoi = Hanoi.new
+# hanoi.play
--- spec/Towers_spec.rb
+++ spec/Towers_spec.rb
@@ -1,7 +1,7 @@
 require "rspec"
-require "Towers"
+require "Hanoi"
 
-describe "#towers" do
+describe "Hanoi" do
     let(:hanoi){Hanoi.new}
     describe "initialize" do
         it "sets the towers up" do
@@ -11,11 +11,38 @@ describe "#towers" do
         end
     end
 
-    describe "move" do
+    describe "#move" do
         it "moves last element from one tower to another" do
             hanoi.move(0, 1)
             expect(hanoi.towers[1]).to eq([1])
         end
 
+        it "doesn't move if one element is greater than another" do
+            hanoi.towers[2] = [5,4,3]
+            hanoi.towers[1] = [6]
+            
+            expect{hanoi.move(1,2)}.to raise_error(StandardError)
+        end
+
+        #check to see if pushing to empty array is an issue
+        it "works properly when you move to an empty array" do
+            hanoi.move(0, 2)
+            expect(hanoi.towers[2]).to eq([1])
+        end
+        
+        #taking from an empty should raise error
+        it "works properly when you move from an empty array" do
+            expect{hanoi.move(1,2)}.to raise_error(StandardError)
+        end
+        #
+    end
+
+    describe "#won?" do
+        it "checks if you won" do 
+            hanoi.towers[2] = [8,7,6,5,4,3,2,1]
+            expect(hanoi.won?).to eq(true)
+        end
+
+        # returns false if you have not yet won
     end
 end
